29ee3de6cc5076b7a94b565b30e255f6b8249eb1
Derek Jones <104667003+SoftwareArchitect9817@users.noreply.github.com>
2022-06-01 12:02:33 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import Home from "./Home.svelte";
+	import Video from "./Video.svelte";
+	export let url = "http://localhost:5000";
+</script>
+
+<Router url="{url}">
+	<header>
+		<h1>History</h1>
+
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="video">Video</Link>
+		</nav>
+	</header>
+
+	<main>
+		<Route path="/" component={Home} />
+		<Route path="/video" component={Video} />
+	</main>
+</Router>
